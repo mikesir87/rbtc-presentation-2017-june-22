2cc61b3d6623a8d00ec73c777013622a4964b144
Michael Irwin <mikesir87@gmail.com>
2017-06-22 00:52:48 -0400

    Added note to remark.js

--- src/remark.js
+++ src/remark.js
@@ -1,3 +1,10 @@
+/**
+ * This is the remark library, but tweaked to include the ability to use
+ * appendTo options in the slide markdown. Beyond that, should all be the
+ * same.
+ */
+
+
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // Copyright Joyent, Inc. and other Node contributors.
 //
